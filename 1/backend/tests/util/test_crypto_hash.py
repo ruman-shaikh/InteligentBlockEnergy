55bd9f7de766f225e9e41d6ff21a0e96fbe6029a
ruman-shaikh <s.k.ruman.0@gmail.com>
2021-03-04 19:34:03 +0530

    Redundent files deleted

--- 1/backend/tests/util/test_crypto_hash.py
+++
@@ -1,6 +0,0 @@
-from backend.util.crypto_hash import crypto_hash
-
-def test_crypto_hash():
-    #It should create the same hash for any order of the same agruments
-    assert crypto_hash(2, "three", [1]) == crypto_hash([1], 2, "three")
-    
